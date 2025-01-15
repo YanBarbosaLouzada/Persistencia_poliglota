73c53e61bb93a0531e0444d56da80f1d36cffb06
YanBarbosaLouzada <yan.barbosalouzada@gmail.com>
2025-01-15 16:49:37 -0300

    Subindo sistema de chat online

--- src/server.js
+++ src/server.js
@@ -9,6 +9,8 @@ import { dirname } from "path";
 import path from "path";
 import { connectDatabase } from "./config/database.js";
 import { config } from "dotenv";
+import { createServer } from "http";
+import { Server } from "socket.io";
 config();
 
 import testRouter from "./routes/TestRoute.js";
@@ -34,6 +36,36 @@ app.use(cors({
     origin: 'http://localhost:3000', // Domínio do front-end
     credentials: true, // Permitir envio de cookies
 }));
+
+const server = createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+    console.log(`socket ${socket.id} connected`);
+    console.log("Usuário conectado!", socket.id);
+    socket.on("disconnect", (reason) => {
+        console.log("Usuário desconectado!", socket.id);
+    });
+    socket.on("set_username", (username) => {
+        socket.data.username = username;
+    });
+    socket.on("message", (text) => {
+        io.emit("receive_message", {
+            text,
+            authorId: socket.id,
+            author: socket.data.username,
+        });
+    });
+});
+
+server.listen(8080, () => {
+    console.log("Socket.IO server running on port 8080");
+});
 
 //colocando rota em uso
 app.use("/exemplo", testRouter);
